f7900841357ca789641e6a72ddc119802d539d3e
Gabriel Sarmiento Lamas <sarmiento.lamas.g.a@gmail.com>
2022-03-03 03:21:41 -0300

    desafio de express agregado productos.txt

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const moment = require("moment");
+const fs = require("fs");
 const app = express();
 const PORT = 8080;
 
+//class
+class Container {
+  constructor() {
+    this.url = `./productos.txt`;
+  }
+  async getAll() {
+    const file = await fs.promises.readFile("./productos.txt", "utf-8"); //se lee el archivo y se retorna su valor
+    return JSON.parse(file);
+  }
+  async getRandom() {
+    const file = await fs.promises.readFile("./productos.txt", "utf-8"); //se lee el archivo
+    const prods = JSON.parse(file);
+    const randomProd = prods[Math.floor(Math.random() * prods.length)]; //se elige un elemento aleatorio del array
+    console.log(randomProd);
+    return randomProd;
+  }
+}
+
+//server
 const server = app.listen(PORT, () => {
   console.log(`Conectado en puerto ${server.address().port}`);
 });
 server.on("error", (error) => console.log("error"));
 
+//get
 app.get("/", (req, res) => {
-  res.send(`<h1>Hola</h1>`);
+  res.send(`<h1>Hola 1</h1>`);
 });
 
-let visita = 0;
-app.get("/visitas", (req, res) => {
-  res.send({ Visita: `${visita++}` });
+app.get("/productos", (req, res) => {
+  lista.getAll().then((li) => res.send(li)); //se ejecuta la función, luego su valor se envía.
 });
 
-app.get("/fhy", (req, res) => {
-  res.send({ fhy: moment().format(`YY/MM/DD HH:mm:ss`) });
+app.get("/productoRandom", (req, res) => {
+  lista.getRandom().then((li) => res.send(li));
 });
+
+// crear nuevo container
+let lista = new Container();
